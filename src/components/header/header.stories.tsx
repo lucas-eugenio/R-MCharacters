2b47fdc04aa845d25e3f12e9dc50a8cbf6029041
Lucas Eugênio <lucasseugenio@hotmail.com>
2022-02-24 14:50:04 -0300

    [v1.0.0] Add README and Minor Changes

--- src/components/header/header.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Header from "./header.component";
-
-export default {
-  title: "Storybook/Header",
-  component: Header,
-};
-
-export const Default = (): React.ReactElement => <Header />;
